a64f466af003aa055834d8e8e3ec04b596764383
soundobj <soundobj@yahoo.com>
2018-08-29 10:45:43 +0100

    real world example flickr api application
    @TODO: refactor to use react instead of jquery once we make purify the codebase

--- app/components/Flickr/Flickr.js
+++ app/components/Flickr/Flickr.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import {
+    curry,
+    compose,
+    prop,
+    map,
+} from 'ramda';
+import $ from 'jquery';
 import template from './Flickr.jsx';
 
 class Flickr extends React.Component {
+    constructor(props) {
+        super(props);
+        this.impure = {
+            getJSON: curry((callback, url) => $.getJSON(url, callback)),
+            setHtml: curry((sel, html) => {
+                // console.error('html', html);
+                $(sel).html(html);
+            }),
+            trace: curry((tag, x) => { console.log(tag, x); return x; }),
+        };
+
+        // pure
+        this.host = 'api.flickr.com';
+        this.path = '/services/feeds/photos_public.gne';
+        this.query = t => `?tags=${t}&format=json&jsoncallback=?`;
+        this.url = t => `https://${this.host}${this.path}${this.query(t)}`;
+        this.img = src => $('<img />', { src });
+        this.mediaUrl = compose(prop('m'), prop('media'));
+        this.mediaToImg = compose(this.img, this.mediaUrl);
+        this.images = compose(map(this.mediaToImg), prop('items'));
+
+        // impure
+        this.append = compose(this.impure.setHtml('#js-main'), this.images);
+        this.app = compose(this.impure.getJSON(this.append), this.url);
+    }
+
+    componentDidMount() {
+        this.app('cats');
+    }
+
     render() {
         this.render = template;
         return this.render.call(this);
--- app/components/Flickr/Flickr.jsx
+++ app/components/Flickr/Flickr.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const render = () => {
     return (
-        <div>Hello Flickr</div>
+        <div>
+            <div>Hello Flickr</div>
+            <div id="js-main" />
+        </div>
     );
 };
 
