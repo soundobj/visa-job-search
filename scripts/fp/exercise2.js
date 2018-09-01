b58b922df2ca2868b54a9397f7bebb97544502a1
soundobj <soundobj@yahoo.com>
2018-09-01 17:33:34 +0100

    either exercises

--- scripts/fp/exercise2.js
+++ scripts/fp/exercise2.js
@@ -1,6 +1,6 @@
 // babel-node scripts/fp/exercise2.js
 // Containers, Functors
-import { curry, compose, map } from 'ramda';
+import { curry, compose, map, concat, add, toString } from 'ramda';
 
 class Container { // AKA identity
     constructor(x) {
@@ -21,7 +21,7 @@ class Maybe {
     static of(x) {
         return new Maybe(x);
     }
-    getIsNothing() {
+    get isNothing() {
         return this.$value === null || this.$value === undefined;
     }
     constructor(x) {
@@ -37,7 +37,7 @@ class Maybe {
 
 
 const nothing = Maybe.of();
-// console.error("nothing", nothing.getIsNothing());
+// console.error("nothing", nothing.isNothing());
 
 
 // withdraw :: Number -> Account -> Maybe(Account)
@@ -50,7 +50,65 @@ const updateLedger = account => account;
 const remainingBalance = balance => balance === null ? null :`Your balance is $${balance.balance}`;
 // finishTransaction :: Account -> String
 const finishTransaction = compose(remainingBalance, updateLedger);
+
+const maybe = curry((v, f, m) => {
+    if (m.isNothing) {
+        return v;
+    }
+    return f(m.$value);
+});
+
+/*
+*
+*/
+
 // getTwenty :: Account -> Maybe(String)
-const getTwenty = compose(map(finishTransaction), withdraw(20));
-console.error("getTwenty", getTwenty({ balance: 200.00 }));
-// console.error("getTwenty", getTwenty({ balance: 10.00 }));
+const getTwenty = compose(maybe('You\'re broke!', finishTransaction), withdraw(20));
+// console.error("balance 200", getTwenty({ balance: 200.00 }));
+// console.error("balance 10", getTwenty({ balance: 10.00 }));
+
+// EITHER
+
+class Either {
+    static of(x) {
+        return new Right(x);
+    }
+    constructor(x) {
+        this.$value = x;
+    }
+}
+class Left extends Either {
+    map(f) {
+        return this;
+    }
+    inspect() {
+        return `Left(${this.$value})`;
+    }
+}
+class Right extends Either {
+    map(f) {
+        return Either.of(f(this.$value));
+    }
+    inspect() {
+        return `Right(${this.$value})`;
+    }
+}
+const left = x => new Left(x);
+
+const moment = require('moment');
+// getAge :: Date -> User -> Either(String, Number)
+const getAge = curry((now, user) => {
+    const birthDate = moment(user.birthDate, 'YYYY-MM-DD');
+    return birthDate.isValid()
+        ? Either.of(now.diff(birthDate, 'years'))
+        : left('Birth date could not be parsed');
+});
+// console.error("EITHER Right getAge:", getAge(moment(), { birthDate: '2005-12-12' }));
+// console.error("EITHER Left getAge:", getAge(moment(), { birthDate: 'July 4, 2001' }));
+
+// fortune :: Number -> String
+const fortune = compose(concat('If you survive, you will be '), toString, add(1));
+// zoltar :: User -> Either(String, _)
+const zoltar = compose(map(fortune), getAge(moment()));
+console.error("right:", zoltar({ birthDate: '2005-12-12' }));
+console.error("left:", zoltar({ birthDate: 'balloons!' }));
