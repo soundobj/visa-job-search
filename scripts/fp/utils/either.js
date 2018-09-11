f0782ed2efa820a46a12385d20d5b7fd5e48e126
soundobj <soundobj@yahoo.com>
2018-09-11 17:13:25 +0300

    Chapter 8  documents either function

--- scripts/fp/utils/either.js
+++ scripts/fp/utils/either.js
@@ -1,5 +1,13 @@
 import { curry } from 'ramda';
-// either :: (a -> c) -> (b -> c) -> Either a b -> c
+/**
+ * @function either
+ * @param {function} f the function to apply to the value e if isRightValue
+ * @param {function} g the function to apply to the value e  if not isRightValue
+ * @param {Maybe} e value inside the Maybe
+ * @requires https://www.npmjs.com/package/ramda
+ * @requires https://www.npmjs.com/package/monet
+ * either :: (a -> c) -> (b -> c) -> Either a b -> c
+ */
 export default curry((f, g, e) => {
   let result;
   switch (e.isRightValue) {
